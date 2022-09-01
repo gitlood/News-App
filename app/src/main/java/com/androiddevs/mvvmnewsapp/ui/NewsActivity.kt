70b8ac7133b072186affab4de4f19e099b745ca7
gitlood <unilood@gmail.com>
2022-09-02 11:38:03 +1200

    No internet connection handling

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -19,7 +19,8 @@ class NewsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_news)
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
-        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository = newsRepository)
+        val viewModelProviderFactory =
+            NewsViewModelProviderFactory(application, newsRepository = newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         bottomNavigationView.setupWithNavController(navController = newsNavHostFragment.findNavController())
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
@@ -1,13 +1,15 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 
 class NewsViewModelProviderFactory(
+    val application: Application,
     val newsRepository: NewsRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return NewsViewModel(newsRepository = newsRepository) as T
+        return NewsViewModel(application, newsRepository = newsRepository) as T
     }
 }
